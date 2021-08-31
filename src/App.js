48e527aa56627a73385c8760306195a39240bd40
Putra Surya <putra.surya.h69@gmail.com>
2021-09-01 06:10:17 +0700

    responsive display

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import AppTimer from "./components/AppTimer";
 function App() {
   return (
     <div className="padding-left-3 padding-right-3">
-      <Row className="padding-top-2" gutter={50}>
-        <Col span={8}>
+      <Row className="padding-top-2" gutter={[50, 50]}>
+        <Col span={24} xs={{ order: 2 }} lg={{ order: 1, span: 12 }} xl={8}>
           <AppReminder />
         </Col>
-        <Col span={8}>
+        <Col span={24} xs={{ order: 1 }} lg={{ order: 1, span: 12 }} xl={8}>
           <AppTimer />
         </Col>
-        <Col span={8}>
+        <Col span={24} xs={{ order: 3 }} lg={{ order: 1, span: 12 }} xl={8}>
           <AppEmbed />
         </Col>
       </Row>
