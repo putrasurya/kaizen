df4137ba1b529c00eda9c50195a8e3a7fd52d0bc
putra surya herlambang <putra.surya.h69@gmail.com>
2022-07-16 04:06:38 +0700

    Refactor and Enhance the Countdown (#26)
    * refactor
    
    * refactor countdown
    
    * refactor and add tooltip to countdown
    
    Co-authored-by: Putra Herlambang <Putra.Herlambang@iongroup.com>

--- src/components/TimerCountdown.jsx
+++ src/components/TimerCountdown.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Typography, Space } from "antd";
+import { Typography, Space, Tooltip } from "antd";
 import { useEffect, useState, useRef, useContext } from "react";
 import { store } from "../redux/store";
 
@@ -17,38 +17,54 @@ function Buzz() {
   document.getElementById("root").append(audio);
 }
 
-function Countdown({ id, play, seconds, secondsSpent, setPlay, className }) {
+export default function Countdown({ id, play, seconds, secondsSpent, setPlay, className }) {
   const { updateSecondsSpent } = useContext(store);
   const [hour, setHour] = useState(0);
   const [minute, setMinute] = useState("00");
   const [second, setSecond] = useState("00");
-  const spent = useRef((secondsSpent || 0) * 1000);
+  const [tooltipDisplay, setTooltipDisplay] = useState("0.00/0.00");
+  const spent = useRef(secondsToMillis(secondsSpent || 0));
   const timeStart = useRef(null);
 
   useEffect(() => {
-    if (false === play) {
-      timeStart.current = null;
-      updateSecondsSpent(id, Math.floor(spent.current / 1000));
-    } else {
-      timeStart.current = new Date();
-    }
+    playPropChanged();
     tiktok();
   }, [play]);
 
   useEffect(() => {
+    secondsSpentPropChanged();
+  }, [secondsSpent])
+
+  function playPropChanged() {
+    if (false === play) {
+      timeStart.current = null;
+      updateSecondsSpent(id, millisToSeconds(spent.current));
+    } else {
+      timeStart.current = new Date();
+    }
+  }
+
+  function secondsSpentPropChanged() {
     if (secondsSpent === 0 && false === play) {
       timeStart.current = new Date();
       spent.current = 0;
       tiktok();
     }
-  }, [secondsSpent])
+  }
+
+  function updateTooltipDisplay() {
+    const [originHour, originMinute] = extractToHourMinuteAndSecondWithPadZero(seconds);
+    setTooltipDisplay(`${hour}.${minute} / ${originHour}.${originMinute}`)
+  }
 
   function tiktok() {
-    if (spent.current >= seconds * 1000) {
-      if (spent.current > seconds * 1000) {
+    updateTooltipDisplay();
+
+    if (spent.current >= secondsToMillis(seconds)) {
+      if (spent.current > secondsToMillis(seconds)) {
         setHour((_) => 0);
-        setMinute((_) => String("00"));
-        setSecond((_) => String("00"));
+        setMinute((_) => "00");
+        setSecond((_) => "00");
         updateSecondsSpent(id, seconds);
         if (play === true) {
           Buzz();
@@ -58,22 +74,22 @@ function Countdown({ id, play, seconds, secondsSpent, setPlay, className }) {
       return;
     }
 
-    const secs = seconds - Math.floor(spent.current / 1000);
-    const h = Math.floor(secs / 3600);
-    const m = Math.floor((secs % 3600) / 60);
-    const s = secs % 60;
+    const secondsLeft = seconds - millisToSeconds(spent.current);
+    const [hour, minute, second] = extractToHourMinuteAndSecondWithPadZero(secondsLeft);
 
-    setHour((_) => h);
-    setMinute((_) => String(m).padStart(2, "0"));
-    setSecond((_) => String(s).padStart(2, "0"));
+    setHour((_) => hour);
+    setMinute((_) => minute);
+    setSecond((_) => second);
 
+    // stop when it's not playing
     if (!play || null === timeStart.current) return;
 
-    const accumulation = new Date().getTime() - timeStart.current.getTime();
-    spent.current = secondsSpent * 1000 + accumulation;
+    const realtimeGap = new Date().getTime() - timeStart.current.getTime();
+    spent.current = secondsSpent * 1000 + realtimeGap;
 
-    if (Math.floor(spent.current / 1000) % 60 === 0) {
-      updateSecondsSpent(id, Math.floor(spent.current / 1000));
+    // update store when it's rounded to a minute
+    if (millisToSeconds(spent.current) % 60 === 0) {
+      updateSecondsSpent(id, millisToSeconds(spent.current));
     }
 
     setTimeout(() => tiktok(), 1000);
@@ -81,12 +97,32 @@ function Countdown({ id, play, seconds, secondsSpent, setPlay, className }) {
 
   return (
     <Space>
-      <Title level={3} className={className}>
-        {hour}.{minute}
-        <small style={{ fontWeight: 300, fontSize: "0.7em" }}>.{second}</small>
-      </Title>
+      <Tooltip title={tooltipDisplay}>
+        <Title level={3} className={className}>
+          {hour}.{minute}
+          <small style={{ fontWeight: 300, fontSize: "0.7em" }}>.{second}</small>
+        </Title>
+      </Tooltip>
     </Space>
   );
 }
 
-export default Countdown;
+function extractToHourMinuteAndSecond(secs) {
+    const hour = Math.floor(secs / 3600);
+    const minute = Math.floor((secs % 3600) / 60);
+    const second = secs % 60;
+    return [hour, minute, second];
+}
+
+function extractToHourMinuteAndSecondWithPadZero(secs) {
+  const [hour, minute, second] = extractToHourMinuteAndSecond(secs);
+  return [hour, String(minute).padStart(2, "0"), String(second).padStart(2, "0")];
+}
+
+function millisToSeconds(millis) {
+  return Math.floor(millis/1000);
+}
+
+function secondsToMillis(seconds) {
+  return seconds*1000;
+}
